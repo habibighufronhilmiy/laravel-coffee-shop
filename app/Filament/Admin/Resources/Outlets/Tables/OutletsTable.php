1c2d4e2675725a50690ecdbd74a8e733dd9a9a3b
habibighufronhilmiy <habibighufron11@gmail.com>
2026-05-30 17:22:17 +0700

    fix: add delete action button to outlet list table

--- app/Filament/Admin/Resources/Outlets/Tables/OutletsTable.php
+++ app/Filament/Admin/Resources/Outlets/Tables/OutletsTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Admin\Resources\Outlets\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
@@ -28,6 +29,7 @@ class OutletsTable
             ->filters([])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
